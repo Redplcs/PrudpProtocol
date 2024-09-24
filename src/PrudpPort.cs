a7bfaca08a3807c5602634881c7f067ec41bf6d8
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-09-24 13:42:39 +0500

    Encode virtual port to byte

--- src/PrudpPort.cs
+++ src/PrudpPort.cs
@@ -2,18 +2,27 @@
 
 internal record struct PrudpPort
 {
+	private const byte StreamIdMask = 0xF;
+	private const byte StreamTypeShiftBy = 0x4;
+
 	public byte StreamId;
 	public PrudpStreamType StreamType;
 
+	public readonly byte Encode()
+	{
+		var streamIdMasked = StreamId & StreamIdMask;
+		var streamTypeInt = (int)StreamType;
+
+		// Shifting applies to integer, so casting streamTypeInt to byte adds excess casting to int.
+		return (byte)((streamTypeInt << StreamTypeShiftBy) | streamIdMasked);
+	}
+
 	public static PrudpPort Decode(byte b)
 	{
-		const byte streamIdMask = 0xF;
-		const byte streamTypeShiftBy = 0x4;
-
 		return new PrudpPort
 		{
-			StreamId = (byte)(b & streamIdMask),
-			StreamType = (PrudpStreamType)(b >> streamTypeShiftBy)
+			StreamId = (byte)(b & StreamIdMask),
+			StreamType = (PrudpStreamType)(b >> StreamTypeShiftBy)
 		};
 	}
 }
--- tests/PrudpPortTests.cs
+++ tests/PrudpPortTests.cs
@@ -3,6 +3,25 @@
 public class PrudpPortTests
 {
 	[Theory]
+	[InlineData(1, PrudpStreamType.Secure, 0x31)]
+	[InlineData(15, PrudpStreamType.Secure, 0x3f)]
+	public void Encode_FromStreamIdAndType_ReturnsExpectedEncodedByte(
+		byte streamId,
+		PrudpStreamType streamType,
+		byte expectedEncodedByte)
+	{
+		var port = new PrudpPort
+		{
+			StreamId = streamId,
+			StreamType = streamType,
+		};
+
+		var encodedByte = port.Encode();
+
+		Assert.Equal(expectedEncodedByte, encodedByte);
+	}
+
+	[Theory]
 	[InlineData(0x31, 1, PrudpStreamType.Secure)]
 	[InlineData(0x3f, 15, PrudpStreamType.Secure)]
 	public void Decode_FromEncoded_ReturnsExpectedStreamIdAndType(
