96da668674a402fb5d0d0c8e7dce0936e95c8123
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-09-24 17:43:35 +0500

    Encode packet types and flags to byte

--- src/PrudpTypeAndFlags.cs
+++ src/PrudpTypeAndFlags.cs
@@ -5,6 +5,14 @@ internal static class PrudpTypeAndFlags
 	private const byte TypeMask = 0b111;
 	private const byte FlagsShiftBy = 3;
 
+	public static byte Encode(PrudpPacketType type, PrudpPacketFlags flags)
+	{
+		var typeMasked = (int)type & TypeMask;
+		var flagsInt = (int)flags;
+
+		return (byte)(flagsInt << FlagsShiftBy | typeMasked);
+	}
+
 	public static (PrudpPacketType type, PrudpPacketFlags flags) Decode(byte b)
 	{
 		var type = (PrudpPacketType)(b & TypeMask);
--- tests/PrudpTypeAndFlagsTests.cs
+++ tests/PrudpTypeAndFlagsTests.cs
@@ -3,6 +3,18 @@
 public class PrudpTypeAndFlagsTests
 {
 	[Theory]
+	[InlineData(PrudpPacketType.Syn, PrudpPacketFlags.NeedAck, 0x20)]
+	public void Encode_FromTypeAndFlags_ReturnsExpectedEncodedByte(
+		PrudpPacketType type,
+		PrudpPacketFlags flags,
+		byte expectedEncodedByte)
+	{
+		var encodedByte = PrudpTypeAndFlags.Encode(type, flags);
+
+		Assert.Equal(expectedEncodedByte, encodedByte);
+	}
+
+	[Theory]
 	[InlineData(0x20, PrudpPacketType.Syn, PrudpPacketFlags.NeedAck)]
 	public void Decode_FromEncoded_ReturnsExpectedTypeAndFlags(
 		byte encoded,
