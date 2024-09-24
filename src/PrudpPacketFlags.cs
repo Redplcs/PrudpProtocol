676e0c9fa08546a3d69e5590934cce31a71f2260
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-09-24 17:45:18 +0500

    Move PrudpPacketFlags from PrudpPacketType.cs to its own file

---
+++ src/PrudpPacketFlags.cs
@@ -0,0 +1,10 @@
+﻿namespace Redplcs.PrudpProtocol;
+
+public enum PrudpPacketFlags : byte
+{
+	None = 0,
+	Ack = 1,
+	Reliable = 2,
+	NeedAck = 4,
+	HasSize = 8,
+}
--- src/PrudpPacketType.cs
+++ src/PrudpPacketType.cs
@@ -4,12 +4,3 @@ public enum PrudpPacketType : byte
 {
 	Syn = 0,
 }
-
-public enum PrudpPacketFlags : byte
-{
-	None = 0,
-	Ack = 1,
-	Reliable = 2,
-	NeedAck = 4,
-	HasSize = 8,
-}
