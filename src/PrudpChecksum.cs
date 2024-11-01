6a35975d37e629872ef8bb5d00cdf1b6ca6d2264
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-01 17:16:59 +0300

    Calculate checksum for data

---
+++ src/PrudpChecksum.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace Redplcs.PrudpProtocol;
+
+internal static class PrudpChecksum
+{
+	public static byte Calculate(ReadOnlySpan<byte> data, ReadOnlySpan<byte> password)
+	{
+		var blocks = MemoryMarshal.Cast<byte, uint>(data);
+		var blocksSum = Sum(blocks);
+		var main = MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref blocksSum, length: 1));
+		var remaining = data[^blocks.Length..];
+
+		var checksum = Sum(password);
+		checksum += Sum(remaining);
+		checksum += Sum(main);
+		return checksum;
+	}
+
+	private static T Sum<T>(ReadOnlySpan<T> data) where T : struct, IAdditionOperators<T, T, T>
+	{
+		T sum = default;
+		foreach (var b in data)
+			sum += b;
+		return sum;
+	}
+}
---
+++ tests/PrudpChecksumTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Redplcs.PrudpProtocol.Tests;
+
+public class PrudpChecksumTests
+{
+	[Fact]
+	public void Calculate_FromHandshakeData_ReturnsExpectedChecksum()
+	{
+		const byte expectedChecksum = 0x3e;
+		var data = new ReadOnlySpan<byte>([0x3f, 0x31, 0x20, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x8, 0x0, 0x0, 0x0]);
+		var password = "7fas5"u8;
+
+		var checksum = PrudpChecksum.Calculate(data, password);
+
+		Assert.Equal(expectedChecksum, checksum);
+	}
+}
