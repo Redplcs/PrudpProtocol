02b5cf20b0f6c84bb0c26bd6e96614e3cdaca991
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-11-01 17:45:01 +0300

    Try read header from sequence of bytes

--- src/PrudpPacketHeader.cs
+++ src/PrudpPacketHeader.cs
@@ -1,4 +1,8 @@
-﻿namespace Redplcs.PrudpProtocol;
+﻿using System.Buffers;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace Redplcs.PrudpProtocol;
 
 internal struct PrudpPacketHeader
 {
@@ -9,4 +13,38 @@ internal struct PrudpPacketHeader
 	public byte SessionId;
 	public uint Signature;
 	public ushort SequenceId;
+
+	public static bool TryRead(ref SequenceReader<byte> reader, out PrudpPacketHeader header)
+	{
+		header = default;
+
+		return
+			TryReadPort(ref reader, out header.SourcePort) &&
+			TryReadPort(ref reader, out header.DestinationPort) &&
+			TryReadTypeAndFlags(ref reader, out header.Type, out header.Flags) &&
+			reader.TryRead(out header.SessionId) &&
+			TryReadUnsigned(ref reader, out header.Signature) &&
+			TryReadUnsigned(ref reader, out header.SequenceId);
+
+		bool TryReadPort(ref SequenceReader<byte> reader, out PrudpPort port)
+		{
+			var read = reader.TryRead(out var value);
+			port = read ? PrudpPort.Decode(value) : default;
+			return read;
+		}
+
+		bool TryReadTypeAndFlags(ref SequenceReader<byte> reader, out PrudpPacketType type, out PrudpPacketFlags flags)
+		{
+			var read = reader.TryRead(out var value);
+			(type, flags) = read ? PrudpTypeAndFlags.Decode(value) : default;
+			return read;
+		}
+
+		bool TryReadUnsigned<T>(ref SequenceReader<byte> reader, out T value) where T : struct, IUnsignedNumber<T>
+		{
+			var read = reader.TryReadExact(count: Marshal.SizeOf<T>(), out var sequence);
+			value = read ? MemoryMarshal.Read<T>(sequence.FirstSpan) : default;
+			return read;
+		}
+	}
 }
---
+++ tests/PrudpPacketHeaderTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Buffers;
+
+namespace Redplcs.PrudpProtocol.Tests;
+
+public class PrudpPacketHeaderTests
+{
+	[Fact]
+	public void TryRead_FromHandshakeData_ReturnsTrue()
+	{
+		var bytes = new byte[] { 0x3f, 0x31, 0x20, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x8, 0x0, 0x0, 0x0 };
+		var sequence = new ReadOnlySequence<byte>(bytes);
+		var reader = new SequenceReader<byte>(sequence);
+
+		var read = PrudpPacketHeader.TryRead(ref reader, out var header);
+
+		Assert.True(read);
+	}
+}
